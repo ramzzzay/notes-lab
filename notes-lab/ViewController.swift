ff361a0c14ef9c28c13ea4994103c5bce1e79fe3
Anton Burkin <ramzzzay@gmail.com>
2018-09-23 12:55:00 +0300

    Initial Commit

---
+++ notes-lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notes-lab
+//
+//  Created by Anton on 9/23/18.
+//  Copyright © 2018 ramzayCO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
