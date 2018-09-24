b22a4aed15f1331363161c7f407c120eef8db6ed
Anton Burkin <ramzzzay@gmail.com>
2018-09-24 12:37:37 +0300

    Add note entity

---
+++ notes-lab/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  notes-lab
+//
+//  Created by Anton on 9/23/18.
+//  Copyright © 2018 ramzayCO. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+    var content = NSAttributedString()
+}
